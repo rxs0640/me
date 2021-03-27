0155a689ea4fb9ade3c948f78de8b58a52d3e53d
rxs0640@hotmail.com <rxs0640@hotmail.com>
2021-03-27 07:34:16 -0700

    Updated routes for single page site

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Me from './components/Me';
-import Projects from './components/Projects';
-import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/me' exact component={Me}/>
-        <Route path='/projects' exact component={Projects}/>
-        <Route path='/about' exact component={About}/>
       </Switch>
     </Router>
   );
