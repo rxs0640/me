7f8fb1d1ad0d33298fd8e94add3bf70e3efb4f1a
rxs0640@hotmail.com <rxs0640@hotmail.com>
2021-03-27 12:05:06 -0700

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './css/Footer.css'
+
+class Footer extends React.Component {
+
+    render() {
+        return(
+            <footer className='page-footer'>
+                <div>
+                    
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
